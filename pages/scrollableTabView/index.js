43bb0ce3eba8e44d38a0f55127a5d60ad1ca96f8
Jason <chenzjchina@gmail.com>
2017-01-12 17:14:03 +0800

    init
    scroll

---
+++ pages/scrollableTabView/index.js
@@ -0,0 +1,86 @@
+Page({
+  data:{
+    tabs: ['收入', '支出', '通知','我的'],
+    stv: {
+      windowWidth: 0,
+      lineWidth: 0,
+      offset: 0,
+      tStart: false
+    },
+    activeTab: 0
+  },
+  onLoad:function(options){
+   try {
+      let {tabs} = this.data; 
+      var res = wx.getSystemInfoSync()
+      this.windowWidth = res.windowWidth;
+      this.data.stv.lineWidth = this.windowWidth / this.data.tabs.length;
+      this.data.stv.windowWidth = res.windowWidth;
+      this.setData({stv: this.data.stv})
+      this.tabsCount = tabs.length;
+    } catch (e) {
+    }
+  },
+  handlerStart(e) {
+    let {clientX, clientY} = e.touches[0];
+    this.startX = clientX;
+    this.tapStartX = clientX;
+    this.data.stv.tStart = true;
+    this.tapStartTime = e.timeStamp;
+    this.setData({stv: this.data.stv})
+  },
+  handlerMove(e) {
+    let {clientX, clientY} = e.touches[0];
+    let {stv} = this.data;
+    let offsetX = this.startX - clientX;
+    this.startX = clientX;
+    stv.offset += offsetX;
+    if(stv.offset <= 0) {
+      stv.offset = 0;
+    } else if(stv.offset >= stv.windowWidth*(this.tabsCount-1)) {
+      stv.offset = stv.windowWidth*(this.tabsCount-1);
+    }
+    this.setData({stv: stv});
+  },
+  handlerCancel(e) {
+
+  },
+  handlerEnd(e) {
+    let {clientX, clientY} = e.changedTouches[0];
+    let endTime = e.timeStamp;
+    let {tabs, stv, activeTab} = this.data;
+    let {offset, windowWidth} = stv;
+    //快速滑动
+    if(endTime - this.tapStartTime <= 300) {
+      //向左
+      if(this.tapStartX - clientX > 5) {
+        if(activeTab < this.tabsCount -1) {
+          this.setData({activeTab: ++activeTab})
+        }
+      } else {
+        if(activeTab > 0) {
+          this.setData({activeTab: --activeTab})
+        }
+      }
+      stv.offset = stv.windowWidth*activeTab;
+    } else {
+      let page = Math.round(offset/windowWidth);
+      if (activeTab != page) {
+        this.setData({activeTab: page})
+      }
+      stv.offset = stv.windowWidth*page;
+    }
+    stv.tStart = false;
+    this.setData({stv: this.data.stv})
+  },
+  _updateSelectedPage(page) {
+    let {tabs, stv, activeTab} = this.data;
+    activeTab = page;
+    this.setData({activeTab: activeTab})
+    stv.offset = stv.windowWidth*activeTab;
+    this.setData({stv: this.data.stv})
+  },
+  handlerTabTap(e) {
+    this._updateSelectedPage(e.currentTarget.dataset.index);
+  }
+})
