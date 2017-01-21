65bfda51c48d660039fc96e393154474ec1d3326
Jason <chenzjchina@gmail.com>
2017-01-21 17:23:40 +0800

    解决嵌套scroll-view Y滚动的bug

--- pages/scrollableTabView/index.js
+++ pages/scrollableTabView/index.js
@@ -25,6 +25,7 @@ Page({
     let {clientX, clientY} = e.touches[0];
     this.startX = clientX;
     this.tapStartX = clientX;
+    this.tapStartY = clientY;
     this.data.stv.tStart = true;
     this.tapStartTime = e.timeStamp;
     this.setData({stv: this.data.stv})
@@ -53,16 +54,25 @@ Page({
     //快速滑动
     if(endTime - this.tapStartTime <= 300) {
       //向左
-      if(this.tapStartX - clientX > 5) {
-        if(activeTab < this.tabsCount -1) {
-          this.setData({activeTab: ++activeTab})
+      if(Math.abs(this.tapStartY - clientY) < 50) {
+        if(this.tapStartX - clientX > 5) {
+          if(activeTab < this.tabsCount -1) {
+            this.setData({activeTab: ++activeTab})
+          }
+        } else {
+          if(activeTab > 0) {
+            this.setData({activeTab: --activeTab})
+          }
         }
+        stv.offset = stv.windowWidth*activeTab;
       } else {
-        if(activeTab > 0) {
-          this.setData({activeTab: --activeTab})
+        //快速滑动 但是Y距离大于50 所以用户是左右滚动
+        let page = Math.round(offset/windowWidth);
+        if (activeTab != page) {
+          this.setData({activeTab: page})
         }
+        stv.offset = stv.windowWidth*page;
       }
-      stv.offset = stv.windowWidth*activeTab;
     } else {
       let page = Math.round(offset/windowWidth);
       if (activeTab != page) {
